027996addebaa7e52e01a57233a1c814edaeb6c3
김미진 <kgmi2010@naver.com>
2025-05-01 14:16:00 +0900

    feat : 메뉴바 추가 임시

--- app/src/main/java/com/hagoshda/monamo/MainActivity.java
+++ app/src/main/java/com/hagoshda/monamo/MainActivity.java
@@ -1,6 +1,7 @@
 package com.hagoshda.monamo;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -9,12 +10,15 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.PagerSnapHelper;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.SnapHelper;
 
+import com.google.android.material.navigation.NavigationView;
 import com.hagoshda.monamo.Adapter.CalenderAdapter;
 
 import java.util.Calendar;
@@ -26,6 +30,9 @@ public class MainActivity extends AppCompatActivity {
     private CalenderAdapter calenderAdapter;
 
     private ImageButton today_ib;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private ImageButton openDrawerButton;
 
     private Calendar calendar;
 
@@ -39,16 +46,45 @@ public class MainActivity extends AppCompatActivity {
         calendar = Calendar.getInstance();
 
         initView();
+        setNavigationBar();
         setCalenderRecycler();
         detectCalenderRecycler();
-        OnClick();
+        TodayOnClick();
     }
     
     public void setTextViewYearMonth(int year, int month) {
         tvYearMonth.setText(year+"년 " + month+ "월");
     }
 
-    private void OnClick() {
+    private void setNavigationBar() {
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.navigation_view);
+        openDrawerButton = findViewById(R.id.open_drawer_button);
+
+        openDrawerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                // 메뉴 클릭 시 동작
+                if (item.toString().equals("메뉴 1")) {
+                    Toast.makeText(MainActivity.this, "메뉴 1 선택됨", Toast.LENGTH_SHORT).show();
+                } else if (item.toString().equals("메뉴 2")) {
+                    Toast.makeText(MainActivity.this, "메뉴 2 선택됨", Toast.LENGTH_SHORT).show();
+                }
+
+                drawerLayout.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
+    }
+
+    private void TodayOnClick() {
         today_ib.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
